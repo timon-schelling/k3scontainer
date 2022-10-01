25a2c551b57306f6e6eff2405693330ee12d1366
Timon Schelling <timon.schelling@outlook.de>
2022-10-02 00:44:41 +0200

    -

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
+pub mod ops;
 pub mod cli;
+
+use const_concat::const_concat;
+
+const K3SCONTAINER_DIR: &str = ".k3scontainer";
+const K3SCONTAINER_CLUSTER_NAME_FILE: &str = const_concat!("K3SCONTAINER_DIR","/name");
---
+++ src/ops.rs
@@ -0,0 +1,78 @@
+use std::fmt;
+use std::fs::File;
+use std::io::{self, prelude::*};
+use std::path::Path;
+use std::process::Command;
+
+#[derive(Debug)]
+pub enum ExecError {
+    NoneZeroExitCode {
+        exitCode: i32,
+        stdout: String,
+        stderr: String,
+    },
+    IoError(io::Error),
+}
+
+impl std::error::Error for ExecError {}
+
+impl fmt::Display for ExecError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ExecError::NoneZeroExitCode {
+                exitCode,
+                stdout: _,
+                stderr: _,
+            } => write!(f, "NoneZeroExitCode Error: {exitCode}"),
+            ExecError::IoError(e) => e.fmt(f),
+        }
+    }
+}
+
+fn exec(cmd: &str) -> Result<String, ExecError> {
+    let mut iter = cmd.split_whitespace();
+    let bin = iter.next().unwrap().to_string();
+    let args = iter.map(String::from).collect::<Vec<String>>();
+    match Command::new(bin).args(args).output() {
+        Ok(out) => {
+            if !out.status.success() {
+                return Err(ExecError::NoneZeroExitCode {
+                    exitCode: out.status.code().unwrap_or(1),
+                    stdout: String::from_utf8_lossy(&out.stdout).to_string(),
+                    stderr: String::from_utf8_lossy(&out.stderr).to_string(),
+                });
+            }
+            Ok(String::from_utf8_lossy(&out.stdout).to_string())
+        }
+        Err(e) => Err(ExecError::IoError(e)),
+    }
+}
+
+fn read(path: &str) -> Result<String, io::Error> {
+    let mut f = File::open(Path::new(&path))?;
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+pub fn provision() {
+    println!("{}", exec("docker ps -a").unwrap())
+}
+
+pub fn remove() {}
+
+pub fn logs() {}
+
+pub fn execute() {}
+
+pub fn copy() {}
+
+pub fn refresh() {}
+
+pub fn run() {}
+
+pub fn shell() {}
+
+pub fn kubectl() {}
